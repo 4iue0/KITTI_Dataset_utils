5c3c20efda416fe2f7460a65f0d701bcb1c3fbce
4iue0 <67323712+4iue0@users.noreply.github.com>
2022-10-13 16:05:08 +0900

    Add files via upload

--- make_kittidataset.py
+++ make_kittidataset.py
@@ -12,11 +12,11 @@ if not os.path.isdir('data'):
     os.makedirs('data/rgb/val')
     os.makedirs('data/rgb/test')
 
-for cheetsheet in os.listdir('cheetsheets'):
+for cheetsheet in os.listdir('python/cheetsheets'):
     if cheetsheet == 'train.txt':
         output_dir = 'data/depth/train'
         mode = 'train'
-    elif cheetsheet == 'test.txt':
+    elif cheetsheet == 'val.txt':
         output_dir = 'data/depth/val'
         mode = 'val'
     else:
@@ -25,7 +25,7 @@ for cheetsheet in os.listdir('cheetsheets'):
     
     print(mode)
     i = 0
-    with open(os.path.join('cheetsheets', cheetsheet)) as f:
+    with open(os.path.join('python/cheetsheets', cheetsheet)) as f:
         for rgb_path in tqdm.tqdm(f):
             rgb_path = rgb_path.replace('\n','')
             depth_path = rgb_path.replace('image_02/data', 'proj_depth/groundtruth/image_02')
@@ -33,7 +33,7 @@ for cheetsheet in os.listdir('cheetsheets'):
             if os.path.isfile(depth_path):
                 rgb = cv2.imread(rgb_path)
                 depth = cv2.imread(depth_path)
-                output = function.fill_depth_colorization(rgb, depth)
+                output = function.fill_depth_colorization(rgb, depth[:,:,0])
                 cv2.imwrite(output_dir + '/%08d' %i + '.png', output)
                 cv2.imwrite(output_dir.replace('depth', 'rgb') + '/%08d' %i + '.png', rgb)
                 i += 1
